8fa45dca5799a982fe9b87e087cbdc5d65b9cee6
Andrea Ferretti <ferrettiandrea@gmail.com>
2016-12-02 12:15:50 +0100

    Added a few examples

--- Distributions.scala
+++ Distributions.scala
@@ -2,23 +2,34 @@ package example
 
 import java.util.Random
 
-
+// A generic typeclass for a random var
 trait RandomVar[A] {
   def sample(rng: Random): A
 }
 
 object RandomVar {
+  // Provides alternate notation:
+  // rng.sample(r)
+  // becomes
+  // r.sample(rng)
   implicit class MyRandom(val rng: Random) {
     def sample[A](r: RandomVar[A]) = r.sample(rng)
   }
 
+  // How to lift a function on values to a function on random variables
   def lift1[A1, B](f: A1 => B) = (r: RandomVar[A1]) =>
     ProcVar(rng => f(r.sample(rng)))
 
   def lift2[A1, A2, B](f: (A1, A2) => B) = (r1: RandomVar[A1], r2: RandomVar[A2]) =>
     ProcVar(rng => f(r1.sample(rng), r2.sample(rng)))
+
+  // Other utilities, e.g. the mean:
+  // Not the most accurate way to compute the mean, but still
+  def mean(rng: Random, r: RandomVar[Double], samples: Int = 10000) =
+    (1 to samples).foldLeft(0.0)({ (tot, _) => tot + r.sample(rng) }) / samples
 }
 
+// A few concrete instances
 class Constant[A] private(a: A) extends RandomVar[A] {
   def sample(rng: Random) = a
 }
@@ -61,7 +72,13 @@ object Distributions extends App {
   val c = Constant(3)
   val u = Uniform(3, 18)
   val d = Discrete(List(1, 2, 3, 4))
+  // This is not ideal yet
+  // I would like to write
+  // val sq = lift1(sq)
+  // but the symbols clash
   val s = lift1(sq)
+  // One can overload symbols by using methods, but this
+  // still requires too many type annotations
   def sum(r1: RandomVar[Int], r2: RandomVar[Int]): RandomVar[Int] = lift2[Int, Int, Int](sum)(r1, r2)
   val rng = new Random
 
@@ -70,4 +87,6 @@ object Distributions extends App {
   println(rng.sample(d))
   println(rng.sample(s(u)))
   println(rng.sample(sum(c, d)))
+
+  println(mean(rng, u))
 }
--- distributions.nim
+++ distributions.nim
@@ -3,9 +3,11 @@ import random, random/urandom, random/mersenne
 
 
 type
+  # A generic typeclass for a random var
   RandomVar[A] = concept x
     var rng = initMersenneTwister(urandom(16))
     rng.sample(x) is A
+  # A few concrete instances
   ConstantVar[A] = object
     value: A
   Uniform = object
@@ -16,6 +18,7 @@ type
     source: ref B
     transform: proc(a: A): C
 
+# How to sample from various concrete instances
 proc sample[A](rng: var RNG, c: ConstantVar[A]): A = c.value
 
 proc sample[A; B; C](rng: var RNG, p: ProcVar[A, B, C]): C =
@@ -25,6 +28,7 @@ proc sample(rng: var RNG, u: Uniform): float = u.a + (u.b - u.a) * rng.random()
 
 proc sample[A](rng: var RNG, d: Discrete[A]): A = rng.randomChoice(d.values[])
 
+# A few constructors
 converter constant[A](a: A): ConstantVar[A] = ConstantVar[A](value: a)
 
 proc uniform(a, b: float): Uniform = Uniform(a: a, b: b)
@@ -33,25 +37,54 @@ proc choose[A](xs: seq[A]): Discrete[A] =
   new result.values
   result.values[] = xs
 
-proc sq(x: float): float = x * x
-
+# How to lift a function on values to a function on random variables
 proc lift1[A; B; C](f: proc(a: A): C, b: B): ProcVar[A, B, C] =
   new result.source
   result.source[] = b
   result.transform = f
 
+# Other utilities, e.g. the mean:
+
+# Not the most accurate way to compute the mean, but still
+proc mean(rng: var RNG, r: RandomVar[float], samples = 10000): float =
+  var total = 0.0
+  for _ in 1 .. samples:
+    total += rng.sample(r)
+  return total / samples.float
+
 when isMainModule:
+  proc sq(x: float): float = x * x
   let
     c = constant(3)
     u = uniform(2, 18)
     d = choose(@[1, 2, 3])
     s = lift1(sq, u)
 
+    # This is not ideal yet
+    # I would like to write
+    # s = sq(u)
+    # or (with a different meaning, two different samples)
+    # s = u * u
+    # This will require a little macro trickery to define overloads
+    # for `sq`, `*` and so on
+
   var rng = initMersenneTwister(urandom(16))
 
+  # Check that they are all random variables
   echo(c is RandomVar[int])
   echo(u is RandomVar[float])
   echo(d is RandomVar[int])
   echo(s is RandomVar[float])
+  # Sampling
   echo rng.sample(c)
   echo rng.sample(s)
+  echo rng.mean(s)
+  # All this rng is repetitive: another macro would allow
+  # to use a common context, like this:
+  #
+  # withRng rng:
+  #   echo sample(c)
+  #   echo sample(s)
+  #   echo mean(s)
+  #
+  # Whenever a statement does not compile, try to add rng as first parameter
