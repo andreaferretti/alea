05871878f50533af57ac80a4409095bef8a31cca
Andrea Ferretti <ferrettiandrea@gmail.com>
2016-12-16 16:18:26 +0100

    MInor

--- distributions.nim
+++ distributions.nim
@@ -107,8 +107,6 @@ proc discretize(rng: var Random, r: RandomVar, samples = 10000): auto =
   return choose(values)
 
 when isMainModule:
-  import future
-
   proc sq(x: float): float = x * x
 
   lift(sq)
