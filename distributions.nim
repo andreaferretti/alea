dcd5f8d4ec1d7790ae04a233b98c62a0ef6d38f4
Andrea Ferretti <ferrettiandrea@gmail.com>
2016-12-16 14:20:57 +0100

    Less dependency on BlaxSpirit/random

--- distributions.nim
+++ distributions.nim
@@ -1,12 +1,14 @@
-import sequtils
+import sequtils, random
 import random, random/urandom, random/mersenne
 
 
 type
-  MyRNG = type(initMersenneTwister(urandom(16)))
+  # A random number generator
+  Random = object
+    random: proc(): float
   # A generic typeclass for a random var
   RandomVar[A] = concept x
-    var rng = initMersenneTwister(urandom(16))
+    var rng: Random
     rng.sample(x) is A
   # A few concrete instances
   ConstantVar[A] = object
@@ -19,19 +21,27 @@ type
     source: ref B
     transform: proc(a: A): C
   ClosureVar[A] = object
-    f: proc(rng: var MyRNG): A
+    f: proc(rng: var Random): A
 
 # How to sample from various concrete instances
-proc sample[A](rng: var RNG, c: ConstantVar[A]): A = c.value
+proc sample[A](rng: var Random, c: ConstantVar[A]): A = c.value
 
-proc sample[A; B; C](rng: var RNG, p: ProcVar[A, B, C]): C =
+proc sample[A; B; C](rng: var Random, p: ProcVar[A, B, C]): C =
   p.transform(sample(rng, p.source[]))
 
-proc sample(rng: var RNG, u: Uniform): float = u.a + (u.b - u.a) * rng.random()
+proc sample(rng: var Random, u: Uniform): float = u.a + (u.b - u.a) * rng.random()
 
-proc sample[A](rng: var RNG, d: Discrete[A]): A = rng.randomChoice(d.values[])
+# proc sample[A](rng: var RNG, d: Discrete[A]): A = rng.randomChoice(d.values[])
 
-proc sample[A](rng: var MyRNG, c: ClosureVar[A]): A = c.f(rng)
+proc sample[A](rng: var Random, c: ClosureVar[A]): A = c.f(rng)
+
+# Here we wrap the RNG typeclass
+proc wrap[R: RNG](rng: R): Random =
+  proc inner(): float =
+    var r = rng
+    return r.random()
+
+  result.random = inner
 
 # A few constructors
 converter constant[A](a: A): ConstantVar[A] = ConstantVar[A](value: a)
@@ -62,7 +72,7 @@ proc lift1[A; B; C](f: proc(a: A): C, b: B): ProcVar[A, B, C] =
 # can only do that defining `map` case by case (although we can abstract
 # the body in a template)
 template mapper(f: typed, B: typedesc): auto =
-  proc inner(rng: var MyRNG): B =
+  proc inner(rng: var Random): B =
     f(rng.sample(x))
 
   result.f = inner
@@ -82,8 +92,8 @@ proc map[B](x: Uniform, f: proc(a: float): B): ClosureVar[B] =
 # We can try to extend `map` to more than one source distribution by using
 # tuples of distributions. Unfortunately, here we run in the same problem
 # and need to define one function per pair... :-/
-proc `&`[A](x: Discrete[A], y: Uniform): ClosureVar[(A, float)] =
-  proc inner(rng: var MyRNG): auto =
+proc `&`[A](x: Uniform, y: ClosureVar[A]): ClosureVar[(float, A)] =
+  proc inner(rng: var Random): auto =
     (rng.sample(x), rng.sample(y))
 
   result.f = inner
@@ -91,11 +101,11 @@ proc `&`[A](x: Discrete[A], y: Uniform): ClosureVar[(A, float)] =
 # Other utilities, e.g. the mean:
 
 # Not the most accurate way to compute the mean, but still
-proc mean(rng: var RNG, r: RandomVar[float], samples = 10000): float =
+proc mean(rng: var Random, r: RandomVar[float], samples = 10000): float =
   (1 .. samples).foldl(a  + rng.sample(r), 0.0) / samples.float
 
 # For a more specific types we can have overloads:
-proc mean(rng: var RNG, r: Uniform, samples = 10000): float = (r.b - r.a) / 2.0
+proc mean(rng: var Random, r: Uniform, samples = 10000): float = (r.b - r.a) / 2.0
 
 when isMainModule:
   import typetraits, future
@@ -113,11 +123,11 @@ when isMainModule:
   let
     c = constant(3)
     u = uniform(2, 18)
-    d = choose(@[1, 2, 3])
+    # d = choose(@[1, 2, 3])
     s = sq(u)
-    t = d.map((x: int) => x * x)
-    z = t.map((x: int) => x * x)
-    w = d & u
+    # t = d.map((x: int) => x * x)
+    z = u.map((x: float) => x * x)
+    w = u & z
 
   # I would also like to write
   # (with a different meaning, two different samples)
@@ -125,19 +135,19 @@ when isMainModule:
   # This will require a little macro trickery to define overloads
   # for `sq`, `*` and so on
 
-  var rng = initMersenneTwister(urandom(16))
+  var rng = wrap(initMersenneTwister(urandom(16)))
 
   # Check that they are all random variables
   echo(c is RandomVar[int])
   echo(u is RandomVar[float])
-  echo(d is RandomVar[int])
+  # echo(d is RandomVar[int])
   echo(s is RandomVar[float])
-  echo(t is RandomVar[int])
-  echo(t is RandomVar)
+  # echo(t is RandomVar[int])
+  # echo(t is RandomVar)
   # Sampling
   echo rng.sample(c)
   echo rng.sample(s)
-  echo rng.sample(t)
+  # echo rng.sample(t)
   echo rng.sample(z)
   echo rng.sample(w)
   echo rng.mean(s)
